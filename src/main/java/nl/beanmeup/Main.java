b22c257f1fa5971ea56b1093861cb6626a1269c4
Bas Stoker <bas.stoker83@gmail.com>
2015-12-30 13:14:22 +0100

    UI

--- src/main/java/nl/beanmeup/Main.java
+++ src/main/java/nl/beanmeup/Main.java
@@ -1,14 +1,24 @@
 package nl.beanmeup;
 
-import ratpack.server.RatpackServer;
+        import ratpack.guice.Guice;
+        import ratpack.handlebars.HandlebarsModule;
+        import ratpack.server.BaseDir;
+        import ratpack.server.RatpackServer;
+
+        import java.io.File;
+
+        import static ratpack.handlebars.Template.handlebarsTemplate;
 
 public class Main {
- public static void main(String... args) throws Exception {
-   RatpackServer.start(server -> server 
-     .handlers(chain -> chain
-       .get(ctx -> ctx.render("Hello World!"))
-       .get(":name", ctx -> ctx.render("Hello " + ctx.getPathTokens().get("name") + "!"))     
-     )
-   );
- }
+    public static void main(String... args) throws Exception {
+        RatpackServer.of(spec -> spec.serverConfig(c -> c.baseDir(BaseDir.find()))
+                .registry(Guice.registry(b -> b.module(HandlebarsModule.class)))
+                .handlers(chain -> chain
+                        .get(ctx -> ctx.render(handlebarsTemplate("index.html")))
+                        .get(":templatename.html",
+                                ctx -> ctx.render(handlebarsTemplate(ctx.getPathTokens().get("templatename") + ".html",
+                                        m -> m.put("name", ctx.getPathTokens().get("name") + "!"))))
+                        .files(f -> f.dir("public"))
+                )).start();
+    }
 }
